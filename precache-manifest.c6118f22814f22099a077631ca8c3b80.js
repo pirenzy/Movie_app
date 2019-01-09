fd1b77503251225104a3569fc07365915aa6ee77
myung heon Lee <pirenzy94@gmail.com>
2019-01-09 11:56:48 +0900

    Updates

---
+++ precache-manifest.c6118f22814f22099a077631ca8c3b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c98bccfb974f20febfd0",
+    "url": "/movie_app/static/js/main.c98bccfb.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "c98bccfb974f20febfd0",
+    "url": "/movie_app/static/css/main.a2419b0c.chunk.css"
+  },
+  {
+    "revision": "9faf28da91a09bf02270374bf9c97a25",
+    "url": "/movie_app/index.html"
+  }
+];
